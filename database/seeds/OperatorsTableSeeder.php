7a0341ce6ffa308fec68564e33c2413cb4091d04
omar tarek <omar.tarek@iVAS.local>
2020-09-02 13:23:18 +0200

    done sql

--- database/seeds/OperatorsTableSeeder.php
+++ database/seeds/OperatorsTableSeeder.php
@@ -12,46 +12,37 @@ class OperatorsTableSeeder extends Seeder
      */
     public function run()
     {
-        
+
 
         \DB::table('operators')->delete();
-        
+
         \DB::table('operators')->insert(array (
-            0 => 
+            0 =>
             array (
                 'id' => 1,
-                'name' => 'etisalat',
-                'rbt_sms_code' => '123',
-                'rbt_ussd_code' => '1234',
-                'image' => '1552552540477.png',
+                'title' => 'etisalat',
                 'country_id' => 1,
                 'created_at' => '2019-02-11 13:12:35',
                 'updated_at' => '2019-03-14 08:35:40',
             ),
-            1 => 
+            1 =>
             array (
                 'id' => 4,
-                'name' => 'Vodafone',
-                'rbt_sms_code' => '123',
-                'rbt_ussd_code' => '',
-                'image' => '1552552433218.png',
+                'title' => 'Vodafone',
                 'country_id' => 1,
                 'created_at' => '2019-02-11 15:23:49',
                 'updated_at' => '2019-03-14 08:33:53',
             ),
-            2 => 
+            2 =>
             array (
                 'id' => 5,
-                'name' => 'Orange',
-                'rbt_sms_code' => '123456789',
-                'rbt_ussd_code' => '123',
-                'image' => '1552552570122.png',
+                'title' => 'Orange',
                 'country_id' => 1,
                 'created_at' => '2019-03-14 08:36:10',
                 'updated_at' => '2019-03-14 08:36:10',
             ),
         ));
-        
-        
+
+
     }
 }
--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -12,25 +12,25 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        
+
 
         \DB::table('users')->delete();
-        
+
         \DB::table('users')->insert(array (
-            0 => 
+            0 =>
             array (
                 'id' => 1,
                 'name' => 'super admin',
                 'email' => 'super_admin@ivas.com',
                 'password' => '$2y$10$u2evAW530miwgUb2jcXkTuqIGswxnSQ3DSmX1Ji5rtO3Tx.MtVcX2',
-                'image' => '',
-                'phone' => '01234567890',
+                'profile_img' => '',
+                //'phone' => '01234567890',
                 'remember_token' => 'rZuEmD6bPlK8lMdaoIC1jRvzlLs17XOy5r6MTsGWA1ggFfMGCVaw7bYG3hBQ',
                 'created_at' => '2017-11-09 06:13:14',
                 'updated_at' => '2018-11-26 08:11:50',
             ),
         ));
-        
-        
+
+
     }
 }
