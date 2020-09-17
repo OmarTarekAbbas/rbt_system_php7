f43aa9ae5431f2de869e6b2f7ef18e374c58f41c
mohamed mahmoud <m.mahmoud@ivas.com.eg>
2020-09-17 13:23:03 +0200

    merge with baher

--- resources/views/roadmap/edit.blade.php
+++ resources/views/roadmap/edit.blade.php
@@ -251,11 +251,10 @@ Departments
                     </div>
 
                     <div class="form-group">
-                        <div class="col-sm-9 col-sm-offset-3 col-lg-10 col-lg-offset-2">
-                            <button type="submit" class="btn btn-primary"><i
-                                    class="fa fa-check"></i> Save</button>
-                            <button type="button" class="btn">Cancel</button>
-                        </div>
+                      <div class="col-sm-9 col-sm-offset-3 col-lg-10 col-lg-offset-2" style="justify-content: center;width: 65%;display: flex;">
+                        <button type="submit" class="btn btn-primary"><i class="fa fa-check"></i> Save</button>
+                        <button type="button" class="btn">Cancel</button>
+                      </div>
                     </div>
                 </form>
             </div>
