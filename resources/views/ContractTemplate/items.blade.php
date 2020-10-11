9bce9713bf58d0e3772ef7aec814acbbee348055
yousefashrafi <yousef.ashraf@ivas.com.eg>
2020-10-11 17:11:10 +0200

    front padding

--- resources/views/ContractTemplate/items.blade.php
+++ resources/views/ContractTemplate/items.blade.php
@@ -21,15 +21,14 @@ Second Party Types
         <div id="ContractTemplateItems" class="container box-content" style="border: 2px dashed black; margin: 20px 0px">
 
           @forelse ($ContractTemplateItems as $item)
-          <div class="container">
-            <div class="container-fluid">
+          <div class="container text-right">
+            <div class="container-fluid" style="padding: 20px">
               <a data-id="{{$item->id}}" onclick="removeItem({{$item->id}})" type="button" class="btn btn-danger btn-circle remove-item"><i class="fa fa-times" aria-hidden="true"></i></a>
               <a data-id="{{$item->id}}" onclick="showEditModal({{$item->id}})" type="button" class="btn btn-success btn-circle edit-item"><i class="fa fa-pencil" aria-hidden="true"></i></a>
               <div id="{{$item->id}}">
                 {!! $item->item !!}
               </div>
             </div>
-            <hr style="width: 100%; color: black; height: 1px; background-color:black;  border-style: dashed none;" />
           </div>
 
           @empty
--- resources/views/fullcontracts/template.blade.php
+++ resources/views/fullcontracts/template.blade.php
@@ -1,5 +1,5 @@
 @forelse ($template_items as $item)
-<div class="container p-3 text-right  class="container box-content" style="border: 2px dashed black;>
+<div class="container p-3 m-3 text-right container box-content">
   <div class="container-fluid">
     <a data-id="{{$item->id}}" onclick="removeItem({{$item->id}})" type="button" class="btn btn-danger btn-circle remove-item"><i class="fa fa-times" aria-hidden="true"></i></a>
     <a data-id="{{$item->id}}" onclick="showEditModal({{$item->id}})" type="button" class="btn btn-success btn-circle edit-item"><i class="fa fa-pencil" aria-hidden="true"></i></a>
