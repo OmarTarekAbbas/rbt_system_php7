ad473cb1a736f272b2f2eeec7c306f6b11509964
emad.mohamed.ivas <emad@ivas.com.eg>
2020-10-25 10:48:59 +0200

    emad,merge mohamed

--- database/migrations/setting/2020_10_22_150118_add_data_to_settings_table.php
+++ database/migrations/setting/2020_10_22_150118_add_data_to_settings_table.php
@@ -13,11 +13,11 @@ class AddDataToSettingsTable extends Migration
      */
     public function up()
     {
-      DB::statement("INSERT INTO `settings` (`id`, `key`, `value`, `created_at`, `updated_at`, `type_id`, `order`) VALUES
-      (1, 'rbt_notify_date', '5', '2020-10-22 07:05:47', '2020-10-22 07:05:47', 2, NULL),
-      (2, 'content_notify_date', '5', '2020-10-22 07:06:01', '2020-10-22 07:06:01', 2, NULL),
-      (3, 'notifiy_rbt_emails', 'mohammed_hs55@yahoo.com,mh124404@gmail.com', '2020-10-22 08:23:53', '2020-10-22 08:32:53', 2, NULL),
-      (4, 'notifiy_content_emails', 'mohammed_hs55@yahoo.com,mh124404@gmail.com', '2020-10-22 08:24:27', '2020-10-22 08:32:57', 2, NULL);");
+      DB::statement("INSERT INTO `settings` ( `key`, `value`, `created_at`, `updated_at`, `type_id`, `order`) VALUES
+      (  'rbt_notify_date', '5', '2020-10-22 07:05:47', '2020-10-22 07:05:47', 2, NULL),
+      ('content_notify_date', '5', '2020-10-22 07:06:01', '2020-10-22 07:06:01', 2, NULL),
+      ( 'notifiy_rbt_emails', 'mohammed_hs55@yahoo.com,mh124404@gmail.com', '2020-10-22 08:23:53', '2020-10-22 08:32:53', 2, NULL),
+      ( 'notifiy_content_emails', 'mohammed_hs55@yahoo.com,mh124404@gmail.com', '2020-10-22 08:24:27', '2020-10-22 08:32:57', 2, NULL);");
     }
 
     /**
