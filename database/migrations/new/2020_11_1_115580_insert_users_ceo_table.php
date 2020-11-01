1fc8ce89be543e0547041fa298fab58ed7ba10a2
omar tarek <omar.tarek@iVAS.local>
2020-11-01 17:00:07 +0200

    make new user and role

--- database/migrations/new/2020_11_1_115580_insert_users_ceo_table.php
+++ database/migrations/new/2020_11_1_115580_insert_users_ceo_table.php
@@ -26,10 +26,6 @@ class InsertUsersCeoTable extends Migration
         'role_priority' => '0',
       ]);
       $user->assignRole($role);
-
-      // // \DB::statement("INSERT INTO `users` (`id`, `name`, `email`, `password`, `remember_token`, `created_at`, `updated_at`, `profile_img`, `aggregator_id`) VALUES ('20', 'Ceo', 'ceo125@gmail.com', '$password', NULL, '2020-10-15 08:09:19', '2020-10-15 08:09:19', '', NULL)");
-      // \DB::statement("INSERT INTO `roles` (`id`, `name`, `role_priority`, `created_at`, `updated_at`) VALUES ('20', 'ceo', '0', '2020-11-01 14:40:19', '2020-11-01 14:40:19')");
-      // \DB::statement("INSERT INTO `user_has_roles` (`role_id`, `user_id`) VALUES ('20', '20')");
     }
 
     /**
