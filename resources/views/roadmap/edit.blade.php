e8b8c1c43c070a428e2130a960f1ab9ebf5127d4
Baher Talal <baher@ivas.com.eg>
2020-11-05 11:09:31 +0200

    roadmap

--- resources/views/roadmap/edit.blade.php
+++ resources/views/roadmap/edit.blade.php
@@ -5,67 +5,64 @@ Departments
 @section('content')
 @include('errors')
 <style>
+
+
   .z-checkbox input[type="checkbox"] {
-    display: none;
-    appearance: none;
+     display: none;
+     appearance: none;
   }
-
-  .z-checkbox input[type="checkbox"]:checked+label:after {
-    transform: scale(1) rotate(0deg);
+   .z-checkbox input[type="checkbox"]:checked + label:after {
+     transform: scale(1) rotate(0deg);
   }
-
-  .z-checkbox label {
-    position: relative;
-    cursor: pointer;
-    padding-left: 20px;
-    cursor: pointer;
-    font-size: 13px;
-    color: #777;
+   .z-checkbox label {
+     position: relative;
+     cursor: pointer;
+     padding-left: 20px;
+     cursor: pointer;
+     font-size: 13px;
+     color: #777;
   }
-
-  .z-checkbox label:before {
-    content: '';
-    width: 20px;
-    height: 20px;
-    background-color: #eee;
-    border: 1px solid #ccc;
-    position: absolute;
-    left: 0;
-    top: 0;
-    border-radius: 4px;
-    -webkit-border-radius: 4px;
-    -moz-border-radius: 4px;
-    -ms-border-radius: 4px;
-    -o-border-radius: 4px;
+   .z-checkbox label:before {
+     content: '';
+     width: 20px;
+     height: 20px;
+     background-color: #eee;
+     border: 1px solid #ccc;
+     position: absolute;
+     left: 0;
+     top: 0;
+     border-radius: 4px;
+     -webkit-border-radius: 4px;
+     -moz-border-radius: 4px;
+     -ms-border-radius: 4px;
+     -o-border-radius: 4px;
   }
-
-  .z-checkbox label:after {
-    font-family: 'FontAwesome';
-    font-weight: 900;
-    content: "\f00c";
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 20px;
-    height: 20px;
-    background-color: #fff;
-    border: 1px solid #000;
-    color: #000;
-    font-size: 16px;
-    line-height: 18px;
-    text-align: center;
-    transform: scale(0) rotate(360deg);
-    transition: all 0.3s ease-in-out;
-    border-radius: 4px;
-    -webkit-border-radius: 4px;
-    -moz-border-radius: 4px;
-    -ms-border-radius: 4px;
-    -o-border-radius: 4px;
+   .z-checkbox label:after {
+     font-family: 'FontAwesome';
+     font-weight: 900;
+     content: "\f00c";
+     position: absolute;
+     top: 0;
+     left: 0;
+     width: 20px;
+     height: 20px;
+     background-color: #fff;
+     border: 1px solid #000;
+     color: #000;
+     font-size: 16px;
+     line-height: 18px;
+     text-align: center;
+     transform: scale(0) rotate(360deg);
+     transition: all 0.3s ease-in-out;
+     border-radius: 4px;
+     -webkit-border-radius: 4px;
+     -moz-border-radius: 4px;
+     -ms-border-radius: 4px;
+     -o-border-radius: 4px;
   }
-
-  .z-checkbox label:hover {
-    text-decoration: underline !important;
-    font-weight: 700;
+   .z-checkbox label:hover {
+     text-decoration: underline !important;
+     font-weight: 700;
   }
 
 
@@ -79,34 +76,15 @@ Departments
     background: #f7a1a173;
     margin-bottom: 0.99rem !important;
   }
-
-  .width_m_auto {
-    width: 65%;
-  }
-
-  .chosen-container-single {
-    /* width: 85% !important; */
-  }
 </style>
 <div class="row">
-  <div class="col-md-12">
-    <div class="box">
-      <div class="box-title">
-        <h3><i class="fa fa-bars"></i>Edit Road Map Form</h3>
-        <div class="box-tool">
-          <a data-action="collapse" href="#"><i class="fa fa-chevron-up"></i></a>
-          <a data-action="close" href="#"><i class="fa fa-times"></i></a>
-        </div>
-      </div>
-      <div class="box-content">
-        <form class="width_m_auto form-horizontal" action="{{url('roadmaps/'.$roadmap->id)}}" method="post">
-          @method('patch')
-          {{ csrf_field() }}
-          <div class="row">
-            <div class="col-md-12">
-              <div class="box box-red">
-                <div class="box-title">
-                  <h3><i class="fa fa-bars"></i> Event Details</h3>
+    <div class="col-md-12">
+        <div class="box">
+            <div class="box-title">
+                <h3><i class="fa fa-bars"></i>Edit Road Map Form</h3>
+                <div class="box-tool">
+                    <a data-action="collapse" href="#"><i class="fa fa-chevron-up"></i></a>
+                    <a data-action="close" href="#"><i class="fa fa-times"></i></a>
                 </div>
             </div>
             <div class="box-content">
@@ -149,13 +127,18 @@ Departments
                                         </div>
                                     </div>
 
-                  <div class="form-group">
-                    <label for="event_start_date" class="col-xs-3 col-lg-2 control-label"> Event Start Date</label>
-                    <div class="input-group date date-picker col-sm-9 col-lg-10 controls" data-date="12-02-2012" data-date-format="dd-mm-yyyy" style="width: 78%; margin: 0 auto;">
-                      <span class="input-group-addon"><i class="fa fa-calendar"></i></span>
-                      <input type="text" value="{{$roadmap->event_start_date->format('d-m-Y')}}" name="event_start_date" id="event_start_date" placeholder="Event Start Date" data-date="12-02-2012" data-date-format="dd-mm-yyyy" class="form-control date-picker">
-                    </div>
-                  </div>
+                                    <div class="form-group">
+                                        <label for="event_end_date" class="col-xs-3 col-lg-2 control-label"> Event End Date</label>
+                                        <div class="input-group date date-picker col-sm-9 col-lg-10 controls" data-date="12-02-2012" data-date-format="dd-mm-yyyy" style="width: 78%; margin: 0 auto;">
+                                            <span class="input-group-addon"><i class="fa fa-calendar"></i></span>
+                                            <input type="text" value="{{$roadmap->event_end_date->format('d-m-Y')}}" name="event_end_date" id="event_end_date"
+                                                placeholder="Event End Date" data-date="12-02-2012" data-date-format="dd-mm-yyyy"  class="form-control date-picker">
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- END Left Side -->
+                        </div>
 
                         <div class="col-md-8" style="float: none; margin: 0 auto;">
                             <div class="box box-red">
@@ -171,33 +154,30 @@ Departments
                                         </div>
                                     </div>
 
-            <div class="col-md-12">
-              <div class="box box-red">
-                <div class="box-title">
-                  <h3><i class="fa fa-bars"></i> Occasion / Aggregator / Operator</h3>
-                </div>
-                <!-- BEGIN Left Side -->
-                <div class="box-content">
-                  <div class="form-group">
-                    <label for="country_id" class="col-xs-3 col-lg-2 control-label">Country</label>
-                    <div class="col-sm-9 col-lg-10 controls">
-                      {!! Form::select('country_id',$countries,$roadmap->country_id,['class'=>'form-control chosen-rtl' , 'id' => 'country_id' ,'required' => true,'style'=>'height: 48px;'])!!}
-                    </div>
-                  </div>
+                                    <div class="form-group">
+                                        <label for="occasion_id" class="col-xs-3 col-lg-2 control-label">Occasion</label>
+                                        <div class="col-sm-9 col-lg-10 controls">
+                                            {!! Form::select('occasion_id',[],$roadmap->occasion_id,['class'=>'form-control chosen-rtl' , 'id' => 'occasion_id' ,'required' => true,'style'=>'height: 48px;'])!!}
+                                        </div>
+                                    </div>
 
-                  <div class="form-group">
-                    <label for="occasion_id" class="col-xs-3 col-lg-2 control-label">Occasion</label>
-                    <div class="col-sm-9 col-lg-10 controls">
-                      {!! Form::select('occasion_id',[],$roadmap->occasion_id,['class'=>'form-control chosen-rtl' , 'id' => 'occasion_id' ,'required' => true,'style'=>'height: 48px;'])!!}
-                    </div>
-                  </div>
+                                    <div class="form-group">
+                                        <label for="aggregator_id" class="col-xs-3 col-lg-2 control-label">Aggregators</label>
+                                        <div class="col-sm-9 col-lg-10 controls">
+                                            {!! Form::select('aggregator_id',$aggregators,$roadmap->aggregator_id,['class'=>'form-control chosen-rtl' , 'id' => 'aggregator_id' ,'required' => true,'style'=>'height: 48px;'])!!}
+                                        </div>
+                                    </div>
 
-                  <div class="form-group">
-                    <label for="aggregator_id" class="col-xs-3 col-lg-2 control-label">Aggregators</label>
-                    <div class="col-sm-9 col-lg-10 controls">
-                      {!! Form::select('aggregator_id',$aggregators,$roadmap->aggregator_id,['class'=>'form-control chosen-rtl' , 'id' => 'aggregator_id' ,'required' => true,'style'=>'height: 48px;'])!!}
-                    </div>
-                  </div>
+                                    <div class="form-group">
+                                        <label for="operator_id" class="col-xs-3 col-lg-2 control-label">Operator</label>
+                                        <div class="col-sm-9 col-lg-10 controls">
+                                            {!! Form::select('operator_id',[],$roadmap->operator_id,['class'=>'form-control chosen-rtl' , 'id' => 'operator_id' ,'required' => true,'style'=>'height: 48px;'])!!}
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- END Left Side -->
+                        </div>
 
                         <div class="col-md-8" style="float: none; margin: 0 auto;">
                             <div class="box box-red">
@@ -234,11 +214,6 @@ Departments
                             </div>
                         </div>
                     </div>
-                  </div>
-                </div>
-              </div>
-              <!-- END Left Side -->
-            </div>
 
                     <div class="row append-row">
                     @foreach($roadmap->providers as $key => $content)
@@ -256,35 +231,35 @@ Departments
                                         </div>
                                     </div>
 
-                  <div class="form-group content">
-                    <label for="content_id" class="col-xs-3 col-lg-2 control-label">Content</label>
-                    <div class="col-sm-9 col-lg-10 controls">
-                      {!! Form::select('content_id[]',[],null,['class'=>'form-control chosen-rtl' , 'data-content' => $content->pivot->content_id , 'id' => 'content_id' ,'required' => true,'style'=>'height: 48px;'])!!}
+                                    <div class="form-group content">
+                                        <label for="content_id" class="col-xs-3 col-lg-2 control-label">Content</label>
+                                        <div class="col-sm-9 col-lg-10 controls">
+                                            {!! Form::select('content_id[]',[],null,['class'=>'form-control chosen-rtl' , 'data-content' => $content->pivot->content_id , 'id' => 'content_id' ,'required' => true,'style'=>'height: 48px;'])!!}
+                                        </div>
+                                    </div>
+
+                                    <div class="form-group">
+                                      <div class="row track-row" data-tracks="{{$content->pivot->rbt_track_specs}}" style="width: 70%; margin: 0 auto; border-radius: 0.5rem;">
+
+                                      </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- END Left Side -->
+                        </div>
+                    @endforeach
                     </div>
-                  </div>
 
-                  <div class="form-group">
-                    <div class="row track-row" data-tracks="{{$content->pivot->rbt_track_specs}}" style="width: 70%; margin: 0 auto; border-radius: 0.5rem;">
-
+                    <div class="form-group">
+                      <div class="col-sm-9 col-sm-offset-3 col-lg-10 col-lg-offset-2" style="justify-content: center;width: 65%;display: flex;">
+                        <button type="submit" class="btn btn-primary"><i class="fa fa-check"></i> Save</button>
+                        <button type="button" class="btn">Cancel</button>
+                      </div>
                     </div>
-                  </div>
-                </div>
-              </div>
-              <!-- END Left Side -->
+                </form>
             </div>
-            @endforeach
-          </div>
-
-          <div class="form-group">
-            <div class="col-sm-9 col-sm-offset-3 col-lg-10 col-lg-offset-2" style="justify-content: center;width: 65%;display: flex;">
-              <button type="submit" class="btn btn-primary"><i class="fa fa-check"></i> Save</button>
-              <button type="button" class="btn">Cancel</button>
-            </div>
-          </div>
-        </form>
-      </div>
+        </div>
     </div>
-  </div>
 
 </div>
 
