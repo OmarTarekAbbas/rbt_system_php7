11bc37ff3a8888d2030bca397b30913769705f6a
yousefashrafi <yousef.ashraf@ivas.com.eg>
2020-11-08 15:24:30 +0200

    role

--- resources/views/roadmap/actions.blade.php
+++ resources/views/roadmap/actions.blade.php
@@ -1,12 +1,12 @@
 <td class="visible-md visible-lg">
   <div class="btn-group">
-      @if(get_action_icons('report/{report}/edit','get'))
+      @if(get_action_icons('roadmaps/{roadmap}','get'))
       <a class="btn btn-sm show-tooltip btn-primary" href="{{url("roadmaps/$roadmap->roadmap_id")}}" title="view"><i class="fa fa-eye"></i></a>
       @endif
-      @if(get_action_icons('report/{report}/edit','get'))
+      @if(get_action_icons('roadmaps/{roadmap}/edit','get'))
       <a class="btn btn-sm show-tooltip" href="{{url("roadmaps/$roadmap->roadmap_id/edit")}}" title="Edit"><i class="fa fa-edit"></i></a>
       @endif
-      @if(get_action_icons('report/{report}/edit','get'))
+      @if(get_action_icons('roadmaps/{id}/delete','get'))
       <a class="btn btn-sm show-tooltip btn-danger" onclick="return ConfirmDelete();" href="{{url("roadmaps/$roadmap->roadmap_id/delete")}}" title="Delete"><i class="fa fa-trash"></i></a>
       @endif
   </div>
