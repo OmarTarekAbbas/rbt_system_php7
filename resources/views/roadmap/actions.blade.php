515b75939163dde819092e5c0ff494564d6ba7a9
yousefashrafi <yousef.ashraf@ivas.com.eg>
2020-11-08 14:52:30 +0200

    merge omar

--- resources/views/roadmap/actions.blade.php
+++ resources/views/roadmap/actions.blade.php
@@ -1,7 +1,13 @@
 <td class="visible-md visible-lg">
   <div class="btn-group">
+      @if(get_action_icons('report/{report}/edit','get'))
       <a class="btn btn-sm show-tooltip btn-primary" href="{{url("roadmaps/$roadmap->roadmap_id")}}" title="view"><i class="fa fa-eye"></i></a>
+      @endif
+      @if(get_action_icons('report/{report}/edit','get'))
       <a class="btn btn-sm show-tooltip" href="{{url("roadmaps/$roadmap->roadmap_id/edit")}}" title="Edit"><i class="fa fa-edit"></i></a>
+      @endif
+      @if(get_action_icons('report/{report}/edit','get'))
       <a class="btn btn-sm show-tooltip btn-danger" onclick="return ConfirmDelete();" href="{{url("roadmaps/$roadmap->roadmap_id/delete")}}" title="Delete"><i class="fa fa-trash"></i></a>
+      @endif
   </div>
 </td>
