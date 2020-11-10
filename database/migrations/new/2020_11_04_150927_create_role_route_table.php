c4a4af5a1984cc731b491c67cfa3887739e14684
emad.mohamed.ivas <emad@ivas.com.eg>
2020-11-10 17:56:01 +0200

    emad,merge all (mohamed and yousef but not review)

--- database/migrations/new/2020_11_04_150927_create_role_route_table.php
+++ database/migrations/new/2020_11_04_150927_create_role_route_table.php
@@ -13,14 +13,14 @@ class CreateRoleRouteTable extends Migration
      */
     public function up()
     {
-        Schema::create('role_route', function (Blueprint $table) {
-            $table->increments('id');
-            $table->integer('role_id')->unsigned()->index('role_id_2');
-            $table->integer('route_id')->unsigned()->index('route_id_2');
-            $table->foreign('route_id', 'role_route_ibfk_1')->references('id')->on('routes')->onUpdate('CASCADE')->onDelete('CASCADE');
-            $table->foreign('role_id', 'role_route_ibfk_2')->references('id')->on('roles')->onUpdate('CASCADE')->onDelete('CASCADE');
-            $table->timestamps();
-        });
+        // Schema::create('role_route', function (Blueprint $table) {
+        //     $table->increments('id');
+        //     $table->integer('role_id')->unsigned()->index('role_id_2');
+        //     $table->integer('route_id')->unsigned()->index('route_id_2');
+        //     $table->foreign('route_id', 'role_route_ibfk_1')->references('id')->on('routes')->onUpdate('CASCADE')->onDelete('CASCADE');
+        //     $table->foreign('role_id', 'role_route_ibfk_2')->references('id')->on('roles')->onUpdate('CASCADE')->onDelete('CASCADE');
+        //     $table->timestamps();
+        // });
     }
 
     /**
