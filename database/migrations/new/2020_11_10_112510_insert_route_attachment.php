bf2795a28cfc2fc1c19f5dbe359f42900c714c7d
omar tarek <omar.tarek@iVAS.local>
2020-11-10 15:52:23 +0200

    make attachment serverside

--- database/migrations/new/2020_11_10_112510_insert_route_attachment.php
+++ database/migrations/new/2020_11_10_112510_insert_route_attachment.php
@@ -13,7 +13,8 @@ class InsertRouteAttachment extends Migration
      */
     public function up()
     {
-      \DB::statement("INSERT INTO `routes` (`id`, `method`, `route`, `controller_name`, `function_name`, `route_name`, `created_at`, `updated_at`) VALUES (NULL, 'get', 'attachment_all/allData', 'AttachmentController', 'allData', '', '2020-11-04 14:40:11', '2020-11-04 14:40:11')");
+      \DB::statement("INSERT INTO `routes` (`id`, `method`, `route`, `controller_name`, `function_name`, `route_name`, `created_at`, `updated_at`) VALUES (NULL, 'get', 'attachment_all/allData', 'AttachmentController', 'allData', NULL, '2020-11-04 14:40:11', '2020-11-04 14:40:11')");
+      \DB::statement("INSERT INTO `routes` (`id`, `method`, `route`, `controller_name`, `function_name`, `route_name`, `created_at`, `updated_at`) VALUES (NULL, 'get', 'report_all/allDate', 'ReportController', 'allDate', NULL, '2020-11-04 14:40:25', '2020-11-04 14:40:25')");
     }
 
     /**
