a77e9567572bf5304224e501d00e88609b58522f
emad.mohamed.ivas <emad@ivas.com.eg>
2020-12-02 13:10:33 +0200

    emad,edit

--- resources/views/rbt/actions.blade.php
+++ resources/views/rbt/actions.blade.php
@@ -8,7 +8,7 @@
     <a class="btn btn-sm show-tooltip" href="{{url("rbt/$rbt->rbt_id/edit")}}" title="Edit"><i
         class="fa fa-edit"></i></a>
     @endif
-    @if(get_action_icons('rbt/{id}/delete','delete'))
+    @if(get_action_icons('rbt/{id}/delete','get'))
     <a class="btn btn-sm show-tooltip btn-danger" onclick="return ConfirmDelete();"
       href="{{url("rbt/$rbt->rbt_id/delete")}}" title="Delete"><i class="fa fa-trash"></i></a>
     @endif
